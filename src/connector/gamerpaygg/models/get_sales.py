32293155061f408db3656ae48026805fe10495d8
Tom Clesius <tomclesius@gmail.com>
2025-05-21 22:34:40 +0200

    Refactor Sale model in get_sales to enforce required fields and update attribute naming for consistency; change soldAt to sold_at using Pydantic's Field for aliasing.

--- src/connector/gamerpaygg/models/get_sales.py
+++ src/connector/gamerpaygg/models/get_sales.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, RootModel
+from pydantic import BaseModel, Field, RootModel
 
 
 class Sale(BaseModel):
-    floatvalue: float | None = None
-    price: float | None = None
-    soldAt: int | None = None
+    floatvalue: float
+    price: float
+    sold_at: int = Field(alias="soldAt")
 
 
 class Sales(RootModel[dict[str, list[Sale]]]):
