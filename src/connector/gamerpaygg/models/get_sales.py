d8308604576d1d5fa47a6e5037ec91159a6616be
Tom Clesius <tomclesius@gmail.com>
2025-05-21 22:36:14 +0200

    Update Sale model in get_sales to change sold_at attribute type from int to datetime for improved type safety and data integrity.

--- src/connector/gamerpaygg/models/get_sales.py
+++ src/connector/gamerpaygg/models/get_sales.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from pydantic import BaseModel, Field, RootModel
 
 
 class Sale(BaseModel):
     floatvalue: float
     price: float
-    sold_at: int = Field(alias="soldAt")
+    sold_at: datetime = Field(alias="soldAt")
 
 
 class Sales(RootModel[dict[str, list[Sale]]]):
